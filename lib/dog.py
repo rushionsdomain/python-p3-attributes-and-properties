6eb475513f8105c2113ed3644b853a2f6080b531
Gachuri <rushionchegge@gmail.com>
2024-09-25 09:53:10 +0300

    fulfilled 11 tests, 4 failing

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    approved_breeds = ["Pug", "Beagle", "Dalmatian", "Bulldog"]
+
+    def __init__(self, name="", breed="Mutt"):
+        """Initializes a Dog instance with a name and a breed."""
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self.name = name
+        
+        if breed not in self.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+            return
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,14 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = ["Sales", "ITC", "Engineering"]
+
+    def __init__(self, name="", job=""):
+        """Initializes a Person instance with a name and a job."""
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self.name = name.title()
+        
+        if job not in self.approved_jobs:
+            print("Job must be in list of approved jobs.")
+            return
+        self.job = job
